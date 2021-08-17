381a410c636483abb41f988073a5f384784e5dab
Liam Stevens <me@liamstevens.me>
2021-08-17 22:48:31 +1000

    added delay to avoid throttling on table

--- inject-dynamo.py
+++ inject-dynamo.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import os
 import base64
+import time
 
 AWS_ACCESS_ID = os.environ["AWS_ACCESS_ID"]
 AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
@@ -16,25 +17,33 @@ def load_json(file):
 
 def upload_recipe(json_obj):
     #json_obj = json.loads(json_obj)
-    response = client.put_item(
-        TableName='CraftingRecipes',
-        Item={
+    payload = {
             'tradeskill' : {"S": json_obj["tradeskill"]},
             'recipelevel' : {"N": str(json_obj["recipeLevel"])},
             'ingredients' : {"B": base64.b64encode(bytes(str(json_obj["ingredients"]),'utf-8'))},
             'name' : {"S": json_obj["id"]}
         }
+    response = client.put_item(
+        TableName='CraftingRecipes',
+        Item=payload
     )
+    print(payload)
+    print(response)
     return response
 
 def upload_all_recipe(path):
     for e in os.listdir(path):
+        time.sleep(0.05)
         if e.endswith(".json"):
-            upload_recipe(load_json(path+'/'+e))
+            print(e)
+            try:
+                upload_recipe(load_json(path+'/'+e))
+            except Exception as exp:
+                print(e+'\n'+str(exp)+'\n\n')
 
 
 
 if __name__ == "__main__":
     #print(load_json("./nwdb.info/db/recipe/Woodworker_ClothWeaveT3.json"))
     #upload_recipe(load_json("./nwdb.info/db/recipe/Woodworker_ClothWeaveT3.json"))
-    upload_all_recipe('/Users/ljs/Projects/new-world-buddy/nwdb.info/db/recipe')
+    upload_all_recipe('/Users/ljs/Projects/new-world-buddy/nwdb.info/db/recipe/json')
