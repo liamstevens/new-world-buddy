3457ddb8e31a806747433cd51015461dbc1ec898
Liam Stevens <me@liamstevens.me>
2021-08-18 16:44:07 +1000

    finish exception handling in decode function

--- craft-path.py
+++ craft-path.py
@@ -112,6 +112,7 @@ class CraftPath:
                 continue
             except Exception as e:
                 print(f"Unexpected error: {e}")
+                continue
 
     def traverse_recipes(self):
         #for e in self.get_recipes():
