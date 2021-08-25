cb479c130ecc60cff4b27e38f28e972b16ab42f3
Liam Stevens <me@liamstevens.me>
2021-08-25 19:48:46 +1000

    finalised lambda iam and handler in python code

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -58,3 +58,8 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
+}
